0854496b48d05677c23a848080791b6f94632a34
Kakada Ly <lkakada@gmail.com>
2016-12-29 18:23:24 -0800

    Created HelloWorld function

---
+++ helloworld/helloworld.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Create function that greets the user by name, or by saying "Hello, World!" if no name is given
+
+func sayGreet(name: String) -> String{
+    var message: String = ""
+    if (name == ""){
+        message = "Hello, World!"
+    }else{
+        message = "Hello, " + name
+    }
+    return message
+}
+
+//greeting function using optional
+func sayGreeting(greeting: String, toName: String? = nil){
+    if toName != nil{
+        print("\(greeting), \(toName!)")
+    }else{
+        print("\(greeting), World!")
+    }
+}
+print(sayGreet(name: "kakada"))
+print(sayGreet(name: ""))
+
+sayGreeting(greeting: "Hello", toName: "Kakada")
+sayGreeting(greeting: "Hello")
--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -2,4 +2,12 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var someInts : [Int] = [3,43,4]
+var someStrs = [String]()
+
+someInts.append(3)
+someStrs.append("Hello")
+
+someInts += [1]
+someInts.count
+someStrs.count
