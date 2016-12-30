6ad97ed0ca461972f83b52d1bcc74c0e7a83e0bf
Kakada Ly <lkakada@gmail.com>
2016-12-29 21:46:50 -0800

    added function to leap year

--- LeapYear/LeapYear/main.swift
+++ LeapYear/LeapYear/main.swift
@@ -10,20 +10,22 @@ import Foundation
 
 //Write a program that will take a year and report if it is a leap year
 
-//Using if else condition to determine the leap year
-var year = 1990
-if(year%4 == 0){
-    if(year%100 == 0){
-        if(year%400 == 0){
-            print("\(year) is the leap year")
+//Function uses for checking leap year
+func leapCheck(){
+    let year = 1990
+    if(year%4 == 0){
+        if(year%100 == 0){
+            if(year%400 == 0){
+                print("\(year) is the leap year")
+            }else{
+                print("\(year) is not the leap year")
+            }
         }else{
-            print("\(year) is not the leap year")
+            print("\(year) is the leap year")
         }
     }else{
-        print("\(year) is the leap year")
+        print("\(year) is not the leap year")
     }
-}else{
-    print("\(year) is not the leap year")
 }
 
 //Class check leap year
@@ -48,5 +50,8 @@ struct Year {
         return year%4 == 0 && year%100 != 0 || year%400 == 0
     }
 }
+
+//Calling
+leapCheck()
 var years = LeapYear(calendarYear: 1996)
     print(years.isLeapYear)
